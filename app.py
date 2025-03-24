3f810b0337f2d0c0770ffc5214db20752d46de28
Shinlil <lilian.michel.dansac@gmail.com>
2025-03-24 14:05:34 +0100

    Added RAG_MCQ and update UI

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from sentence_transformers import SentenceTransformer
 from huggingface_hub import InferenceClient
 import streamlit as st
-from utils_chunks import load_index_and_chunks
-from utils_chunks import retrieve_chunks
+from utils.chunks import load_index_and_chunks
+from utils.chunks import retrieve_chunks
 from RAG_open_quests import RAG_conv
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 # First thing to call
 st.set_page_config(page_title="RAG Model", layout="wide")
+
+# Load env variables
+load_dotenv()
 
 @st.cache_resource
 def load_embedding_model():
@@ -51,7 +53,7 @@ def rag_model(model_id, client_inference, query, dict_chunks): # , embedding_mod
         system_prompt = "You are an expert in patent laws. You provide both detailed answers and your source (include the name of the document)."
         st.session_state.model_conv_history.append({
             "role": "system",
-            "content": f"{system_prompt}\nYou always answer in the same language as the query and within 500 words sources included.\nYou have access to the following context : {context}"
+            "content": f"Do not show this on your response.\n{system_prompt}\nYou always answer in the same language as the query and within 500 words sources included.\nYou have access to the following context : {context}"
         })
         st.session_state.model_conv_history.append({
             "role": "user",
@@ -77,6 +79,45 @@ def rag_model(model_id, client_inference, query, dict_chunks): # , embedding_mod
 
     return model_res
 
+def rag_MCQ(model_id, client_inference, query, dict_chunks):
+    index = dict_chunks["index"]
+    chunks = dict_chunks["chunks"]
+
+    retrieved_chunks = retrieve_chunks(query, index, chunks, embedding_model)
+
+    context = "\n".join([f"[PDF: {chunk['pdf']} - Page: {chunk['page']}] {chunk['text']}" for chunk in retrieved_chunks])
+
+    # Define system prompt only if the conversation has just started
+    if (len(st.session_state.model_conv_history) == 0):
+        system_prompt = "You are an expert in patent laws. You provide both detailed answers and your source (include the name of the document)."
+        st.session_state.model_conv_history.append({
+            "role": "system",
+            "content": f"Do not show this on your response.\n{system_prompt}\nYou always answer in the same language as the query and within 500 words sources included.\nYou have access to the following context : {context}"
+        })
+        st.session_state.model_conv_history.append({
+            "role": "user",
+            "content": query
+        })
+    else:
+        # No additional context
+        st.session_state.model_conv_history.append({
+            "role": "user",
+            "content": query
+        })
+    model_res = RAG_conv(model_id, client_inference, st.session_state.model_conv_history)
+
+    st.session_state.model_conv_history.append({
+        "role": "assistant",
+        "content": model_res
+    })
+
+    return(model_res)
+
+# For the RAG that is specialized in creating open questions and correcting user
+def rag_create_quest(model_id, client_inference, query, dict_chunks):
+    # TODO
+    pass
+
 # Display conversation history
 def display_conversations():
     for conversation in st.session_state.interface_conv_history:
@@ -86,6 +127,32 @@ def display_conversations():
 def submit():
     st.session_state.input = st.session_state.user_input
     st.session_state.user_input = ''
+
+def reset_conv():
+    st.session_state.interface_conv_history = []
+    st.session_state.model_conv_history = []
+    st.session_state.input = ''
+    st.rerun()  # Rerun the script to reflect changes immediately
+
+def init_session_states():
+    # Session state for conversations for the model
+    if 'model_conv_history' not in st.session_state:
+        st.session_state.model_conv_history = []
+
+    if 'model_id' not in st.session_state:
+        st.session_state.model_id = "mistralai/Mistral-7B-Instruct-v0.3"
+
+    if 'RAG_type' not in st.session_state:
+        st.session_state.RAG_type = "open_questions"
+
+    # Session state for the conversations for the UI
+    if 'interface_conv_history' not in st.session_state:
+        st.session_state.interface_conv_history = []
+
+    if 'input' not in st.session_state:
+        st.session_state.input = ''
+
+init_session_states()
 
 embedding_model = load_embedding_model()
 
@@ -102,23 +169,28 @@ st.write(
     """
 )
 
-# Initialize session state for conversations for the model if it doesn't exist yet
-if 'model_conv_history' not in st.session_state:
-    st.session_state.model_conv_history = []
-
-if 'model_id' not in st.session_state:
-    st.session_state.model_id = "mistralai/Mistral-7B-Instruct-v0.3"
-
-if 'interface_conv_history' not in st.session_state:
-    st.session_state.interface_conv_history = []
-
-if 'input' not in st.session_state:
-    st.session_state.input = ''
+if (st.session_state.RAG_type == "open_questions"):
+    st.write(
+        """
+        Ready to answer any questions you may have.
+        """
+    )
+elif (st.session_state.RAG_type == "MCQ"):
+    st.write(
+        """
+        Specialized in making MCQs.\n
+        First, input a subject and then answer with only the corresponding letter or number.\n
+        You can reset this conversation or open a new page if you want a MCQ for another subject.
+        """
+    )
 
 # Process input when the user submits
 if st.session_state.input:
     # Call the RAG model
-    model_response = rag_model(st.session_state.model_id, client_inference, st.session_state.input, dict_chunks)
+    if (st.session_state.RAG_type == "open_questions"):
+        model_response = rag_model(st.session_state.model_id, client_inference, st.session_state.input, dict_chunks)
+    elif (st.session_state.RAG_type == "MCQ"):
+        model_response = rag_MCQ(st.session_state.model_id, client_inference, st.session_state.input, dict_chunks)
 
     # Store the conversation in session state
     st.session_state.interface_conv_history.append({
@@ -131,9 +203,48 @@ if st.session_state.input:
 # Input box at the bottom
 user_input = st.text_input("Your Input:", key="user_input", on_change=submit)
 
+RAG_choices = {
+    "Answer questions": "open_questions", 
+    "MCQs": "MCQ",
+    "Create questions": "create_questions"
+}
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    btn1 = st.button('Reset Conversation')
+
+with col2:
+    btn2 = st.button('Add pdf')
+
+with col3:
+    btn3 = st.button('Add html')
+
 # Button to reset conversation
-if st.button("Reset Conversation"):
-    st.session_state.interface_conv_history = []
-    st.session_state.model_conv_history = []
-    st.session_state.input = ''
-    st.rerun()  # Rerun the script to reflect changes immediately
+if btn1:
+    reset_conv()
+
+if btn2:
+    # TODO
+    pass
+
+if btn3:
+    # TODO
+    pass
+
+selCol1, _ = st.columns([1, 4])
+
+with selCol1:
+    RAG_option = st.selectbox(
+    label="What functionality do you need?",
+    options=list(RAG_choices.keys()),
+    index=0
+    )
+
+if RAG_option:
+    session_RAG_type = RAG_choices[RAG_option]
+    if session_RAG_type != st.session_state.RAG_type:
+        st.session_state.RAG_type = session_RAG_type
+        reset_conv()
+
+st.write("Currently selected:", RAG_option)
